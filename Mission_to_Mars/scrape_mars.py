9ca0bf7c150ea7aabc28b8c7d2357c2883315cc3
JohnTimmers <john.timmers16@gmail.com>
2021-06-27 17:13:35 -0400

    final code push

---
+++ Mission_to_Mars/scrape_mars.py
@@ -0,0 +1,154 @@
+from splinter import Browser
+from bs4 import BeautifulSoup
+from webdriver_manager.chrome import ChromeDriverManager
+import pandas as pd
+import datetime as dt 
+import time 
+
+
+def scrape():
+
+    # Initiate headless driver for deployment
+    browser = Browser("chrome", executable_path="chromedriver", headless=True)
+    news_title, news_paragraph = mars_news(browser)
+    print(news_title)
+    # Run all scraping functions and store in dictionary.
+    data = {
+        "news_title": news_title,
+        "news_paragraph": news_paragraph,
+        "img_url": img_url,
+        "hemispheres": hemispheres(browser),
+        "mars_weather": mars_weather,
+        "facts": mars_facts(),
+        "last_modified": dt.datetime.now()
+    }
+
+    # Stop webdriver and return data
+    browser.quit()
+    return render_template('index.html', data=data)
+
+def mars_news(browser):
+    url = "https://mars.nasa.gov/news/"
+    browser.visit(url)
+
+    # Get first list item and wait half a second if not immediately present
+    browser.is_element_present_by_css("ul.item_list li.slide", wait_time=0.5)
+
+    html = browser.html
+    news_soup = BeautifulSoup(html, "html.parser")
+
+    try:
+        slide_elem = news_soup.select_one("ul.item_list li.slide")
+        news_title = slide_elem.find("div", class_="content_title").get_text()
+        news_p = slide_elem.find(
+            "div", class_="article_teaser_body").get_text()
+
+    except AttributeError:
+        return None, None
+
+    return news_title=news_title, news_p=news_p
+
+
+def featured_image(browser):
+    url = "https://spaceimages-mars.com/"
+    browser.visit(url)
+
+    # Find and click the full image button
+    full_image_elem = browser.find_by_text(' FULL IMAGE')
+    full_image_elem.click()
+
+    # Find the more info button and click that
+    browser.is_element_present_by_text("more info", wait_time=1)
+    more_info_elem = browser.find_link_by_partial_text("more info")
+    more_info_elem.click()
+
+    # Parse the resulting html with soup
+    html = browser.html
+    img_soup = BeautifulSoup(html, "html.parser")
+
+    # Find the relative image url
+    img = img_soup.find_all('img')[1]
+
+    try:
+        rel_img_url = img.get("src")
+
+    except AttributeError:
+        return None
+
+    # Use the base url to create an absolute url
+    img_url = f"https://www.jpl.nasa.gov{rel_img_url}"
+
+    return render_template('index.html', img_url=img_url)
+
+
+def hemispheres(browser):
+
+    # A way to break up long strings
+    url = (
+        "https://marshemispheres.com/"
+        "results?q=hemisphere+enhanced&k1=target&v1=Mars"
+    )
+
+    browser.visit(url)
+
+    # Click the link, find the sample anchor, return the href
+    hemisphere_image_urls = []
+    for i in range(len(links)-1):
+
+        # Find the elements on each loop to avoid a stale element exception
+        browser.find_by_css("h3")[i].click()
+
+        hemi_data = scrape_hemisphere(browser.html)
+
+        # Append hemisphere object to list
+        hemisphere_image_urls.append(hemi_data)
+
+        # Finally, we navigate backwards
+        browser.back()
+ 
+    return render_template('index.html', hemisphere_image_urls=hemisphere_image_urls)
+
+
+
+
+def scrape_hemisphere(html_text):
+
+    # Soupify the html text
+    hemi_soup = BeautifulSoup(html_text, "html.parser")
+
+    # Try to get href and text except if error.
+    try:
+        title_elem = hemi_soup.find("h2", class_="title").get_text()
+        sample_elem = hemi_soup.find("a", text="Sample").get("href")
+
+    except AttributeError:
+
+        # Image error returns None for better front-end handling
+        title_elem = None
+        sample_elem = None
+
+    hemisphere = {
+        "title": title_elem,
+        "img_url": sample_elem
+    }
+
+    return hemisphere
+
+
+def mars_facts():
+    try:
+        df = pd.read_html("https://galaxyfacts-mars.com/")[0]
+    except BaseException:
+        return None
+
+    df.columns = ["description", "value"]
+    df.set_index("description", inplace=True)
+
+    # Add some bootstrap styling to <table>
+    return df.to_html(classes="table table-striped")
+
+
+if __name__ == "__main__":
+
+    # If running as script, print scraped data
+    print(scrape_all())
